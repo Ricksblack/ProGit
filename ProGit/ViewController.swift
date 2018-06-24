977693688b3db3fa38dc333381f339b686d444fc
Ricardo Moreno <rmm_92@outlook.com>
2018-06-23 23:26:56 -0500

    Integración de lógica Se integra la logica para saludar a la persona

--- ProGit/ViewController.swift
+++ ProGit/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var greetLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func greetPerson(_ sender: UIButton) {
+        if let name = nameTextField.text, name != "" {
+            greetLabel.text = "Hola chingonsisimo \(name)"
+        }
+    }
+    
 }
 
